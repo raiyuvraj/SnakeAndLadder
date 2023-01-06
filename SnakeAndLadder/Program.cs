e58594335529ea6dd071981cae66eb248628dc58
raiyuvraj <yuvirai017@gmail.com>
2023-01-05 17:38:12 -0800

    [Yuvraj]-Single_Player

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Play player = new Play();
+         
+        }
+    }
+}   
